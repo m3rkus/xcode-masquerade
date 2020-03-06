ef05519820e21003514119a2ffd4f2932dd265f1
Roman <roman@letmecode.org>
2020-03-06 20:04:02 +0300

    Initial Commit

---
+++ XCodeMasquerade/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XCodeMasquerade
+//
+//  Created by Roman Anistratenko on 06.03.2020.
+//  Copyright © 2020 m3rk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XCodeMasquerade/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XCodeMasquerade
+//
+//  Created by Roman Anistratenko on 06.03.2020.
+//  Copyright © 2020 m3rk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
